8ea55a9b213540230e729408f5e3d22245d8f8b9
Montaser Mossallem <moun2030@gmail.com>
2017-03-13 01:08:54 +0300

    Live Preview and Docs

---
+++ version-1/assets/js/applications.js
@@ -0,0 +1,222 @@
+
+$(document).ready(function(){
+
+
+  // set scroll bottom as a foxed
+  $('html ,body').animate({
+    scrollTop: $('#contact-us-section').offset().top
+  },1000);
+
+
+/*++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+/*						                       function Contents                                         */
+/*
+    1- Scroll Navigation-bar top and bottom According to section
+    2- Scroll To Target Section
+
+*/
+/*++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+
+/* -------------------------------------------------------------------------------------------
+                                  Loading Page
+--------------------------------------------------------------------------------------------*/
+setTimeout(function(){
+  $('.loading-page').slideUp(function(){
+    // change the boy overflow-y
+    $('body').css({'overflow-y':'auto'});
+    // Animate to top
+    $('html ,body').animate({
+      scrollTop: 0
+    },2000);
+  });
+},5000);
+
+/* -------------------------------------------------------------------------------------------
+                          1-  Scroll the navbar
+--------------------------------------------------------------------------------------------*/
+$(window).scroll(function(){
+    /*Collaps hide the Navigation bar for mobile*/
+    var $navbar = $(".navbar-collapse");
+    $navbar.collapse('hide');
+    /*slide In Down the Navigation and make it as a top with fixed position*/
+    var $navigationBar = $('#navigation-bar');
+    var $targetScropOffset = $('#home-section').height()+120;
+    if($(this).scrollTop() > $targetScropOffset  )
+    $navigationBar.addClass('animated slideInDown navbar-fixed-top');
+    else
+    $navigationBar.removeClass('animated slideInDown navbar-fixed-top');
+});
+
+/* -------------------------------------------------------------------------------------------
+                          2-  Smooth Scroll To Target Section
+--------------------------------------------------------------------------------------------*/
+ $("#navbar-menu a").each(function(){
+   $(this).click(function(){
+     var $targetSection = $(this).attr('href');
+     $('html, body').animate({
+       scrollTop : $($targetSection).offset().top
+     },1000);
+     return false ;
+   });
+ });
+
+ /* -------------------------------------------------------------------------------------------
+                          AOS Library Initialization
+ --------------------------------------------------------------------------------------------*/
+ AOS.init();
+
+ /* -------------------------------------------------------------------------------------------
+                          Slide Effect and open Target Box
+ --------------------------------------------------------------------------------------------*/
+ $('.portfolio-block').each(function(){
+    $(this).click(function(){
+      var $dataProject = $(this);
+      // Check if box open or not { Reset Box and init it }
+       var $targetBox = $('#target-project');
+       if($targetBox.is(':visible')) // it's Open !
+        $targetBox.slideUp();
+
+        // Load each value according to event handler
+        var $projDetails = {
+          'image':$(this).attr('data-image'),
+          'title' :$(this).attr('data-project-title'),
+          'details' :$(this).attr('data-project-details'),
+          'linkPreview' :$(this).attr('data-preview-link'),
+          'screenshotLink'  :$(this).attr('data-screenshot-href'),
+        };
+       // Sotre Data into Target Box TO Display The-info.
+       $('#target-image').attr('src','assets/images/'+$.trim($projDetails.image));
+       $('#target-title').html($.trim($projDetails.title));
+       $('#target-details').html($.trim($projDetails.details));
+       $('#target-preview').attr('href',$.trim($projDetails.linkPreview));
+       $('#target-screenshot').attr('href',$.trim($projDetails.screenshotLink));
+
+
+
+
+      var $completedData =  function() {
+       $targetBox.slideDown();
+       }
+      var $progressData = function() {
+
+        return ;
+      }
+
+      // Animate To Target Box That Will contain The Infos
+      $('html , body').animate({
+        scrollTop: $('#portfolio-title').offset().top ,
+      }, {
+            duration: 700,
+            complete:$completedData,
+            progress:$progressData()
+         });
+    });
+  });
+
+
+   //----------------------------------------------------------------------------------------------
+    //-------------------       Fill Skills  ----------------------------
+    //----------------------------------------------------------------------------------------------
+
+    $('.fill-indicator').bind('inview', function (event, visible) {
+        if (visible == true) {
+          var $skills = $('.fill-indicator');
+
+           $skills.each(function(){
+             // get value from attribute
+              $skillValue = $(this).attr('data-indicator-value') ;
+            // Set Value for text
+           $(this).parent().prev('.skill-elements').children('.indicator-value').text($skillValue) ;
+            // fille the ruller
+            $(this).css({width:$skillValue});
+           });
+        }else
+        $(this).css({width:'0%'});
+    });
+
+    // Profile Image zoom In
+$('.img-shape img , .col-xs-sm-fit-contents').bind('inview', function (event, visible) {
+  if (visible == true) {
+    $(this).addClass('animated zoomIn');
+  }
+});
+
+
+
+
+    //----------------------------------------------------------------------------------------------
+    //-------------------       Form Validating   ----------------------------
+    //----------------------------------------------------------------------------------------------
+    $('input[type="text"], input[type="email"], textarea').keyup(function(){
+     $(this).css({'border':'1px solid #555'});
+     $('.show-result').slideUp(function(){
+       $('.show-result').html(null).css({'color':'#fff'})
+     });
+    });
+    window.SendEmail = function (){
+      // Selectores
+      var $name = $('#full-name');
+      var $email = $('#user-email');
+      var $phoneNumber = $('#user-phone-number');
+      var $subject = $('#Subject');
+      var $message = $('#Message');
+
+
+
+      // Check Fields empty or not
+      if($name.val().length === 0)
+      $name.css({'border':'1px solid red'});
+
+      if($email.val().length === 0)
+      $email.css({'border':'1px solid red'});
+
+      if($subject.val().length === 0)
+      $subject.css({'border':'1px solid red'});
+
+      if($message.val().length === 0)
+      $message.css({'border':'1px solid red'});
+
+      if($phoneNumber.val().length === 0)
+      $phoneNumber.css({'border':'1px solid red'});
+
+
+      // Store Values
+      var dataString = {
+        "full-name":$name.val() ,
+        "user-email":$email.val() ,
+        "user-phone-number":$phoneNumber.val() ,
+        "Subject":$subject.val() ,
+        "Message":$message.val()
+      }
+
+      // Calling Controller
+      $.ajax({
+        type: "POST",
+        url: "php/controller.php",
+        data: dataString,
+        beforeSend:function (){
+          $('.input-container > button').html('Sending ...');
+        },
+        success: function (response) {
+          $('.show-result').slideDown(function(){
+            $(this).html(response);
+            $('.input-container > button').html('Send');
+          });
+
+        }
+      });
+    }
+
+
+
+    // Check about Safari Browser
+    var naviationB = navigator.userAgent.toLowerCase();
+    if (naviationB.indexOf('safari') != -1) {
+          if (naviationB.indexOf('chrome') <= -1) {
+             // Delete cutting border from Image
+             $('.img-block').css('border','0px');
+             $('.client-img img').css('border-radius','0px');
+          }
+      }
+
+});
---
+++ version-1/php/api.php
@@ -0,0 +1,44 @@
+<?php
+// Build A Class to Clean Data
+class Fladina_Apis {
+
+
+  // This line taking about " How many time you will reply to your client email "
+  //-> Email
+    private $email_to = "moun2030@gmail.com";
+  //-> Reply Time
+    private $reply_time = "24 Hours";
+
+
+  // Responsed Message to Ajax
+  private $successMessage = "Your Message has been Sent , I will replay to you within";
+
+  /* Stop script or html tags or anything else */
+  public function Fladina_Stop_Clean_Tags($data) {
+    $data = preg_replace("/javascript/i", "j&#097;v&#097;script",$data);
+    $data = preg_replace("/alert/i", "&#097;lert",$data);
+    $data = preg_replace("/about:/i", "&#097;bout:",$data);
+    $data = preg_replace("/onmouseover/i", "&#111;nmouseover",$data);
+    $data = preg_replace("/onclick/i", "&#111;nclick",$data);
+    $data = preg_replace("/onload/i", "&#111;nload",$data);
+    $data = preg_replace("/onsubmit/i", "&#111;nsubmit",$data);
+    $data = preg_replace("/<body/i", "&lt;body",$data);
+    $data = preg_replace("/<html/i", "&lt;html",$data);
+    $data = preg_replace("/document\./i", "&#100;ocument.",$data);
+    $data = preg_replace("/<script/i", "&lt;&#115;cript",$data);
+    $data = str_replace(' & ', ' &amp; ', $data);
+    return (get_magic_quotes_gpc() ? stripslashes($data) : $data);
+  }
+  /*This Function is Sending an Emails*/
+  public function Fladina_Mailer( $subject , $message , $from ) {
+    $headers = "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-type:text/html; charset=utf-8" . "\r\n";
+    $headers .= "From: <$from>" . "\r\n";
+    return mail($this->email_to , $subject, $message, $headers);
+  }
+  public function Auto_replay_to_message_sent_success (){
+    return $this->successMessage.' '.$this->reply_time;
+  }
+}
+
+?>
